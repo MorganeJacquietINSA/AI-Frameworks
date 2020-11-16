d7e1c121875a3c18865496342d57241315dbd6ab
Morgane Jacquiet <jacquiet@etud.insa-toulouse.fr>
2020-11-16 15:51:45 +0100

    TP4 AIF - Code developpement 1

--- CodeDevelopment/script/learning.py
+++ CodeDevelopment/script/learning.py
@@ -17,7 +17,14 @@ import argparse
 
 # TODO Write here the parameters that can be given as inputs to the algorithm.
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', type=str, default=)
+DATA_PATH = '/Users/cecile/Documents/INSA/5A/AI-Frameworks/CodeDevelopment'
+
+parser.add_argument('--data_dir', type=str, default=DATA_PATH+'/data/')
+parser.add_argument('--results_dir', type=str, default=DATA_PATH+'/results/')
+parser.add_argument('--model-dir', type=str, default=DATA_PATH+'/model/')
+
+parser.add_argument('--epochs', type=int, default=2)
+parser.add_argument('--batch_size', type=int, default=20)
 args = parser.parse_args()
 
 
@@ -31,19 +38,54 @@ N_val = len(os.listdir(args.data_dir+"/validation/cats/")) + len(os.listdir(args
 print("%d   %d"%(N_train, N_val))
 
 # TODO Write here code to generate obh train and validation generator
-train_datagen = kpi.ImageDataGenerator()
+train_datagen = kpi.ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-train_generator = train_datagen.flow_from_directory()
+train_generator = train_datagen.flow_from_directory(
+    args.data_dir + "/train/",  # this is the target directory
+    target_size=(img_width, img_height),
+    batch_size=args.batch_size,
+    class_mode='binary')
 
-valid_datagen = kpi.ImageDataGenerator()
+valid_datagen = kpi.ImageDataGenerator(rescale=1. / 255)
 
-validation_generator = valid_datagen.flow_from_directory()
+validation_generator = valid_datagen.flow_from_directory(
+    args.data_dir + "/validation/",
+    target_size=(img_width, img_height),
+    batch_size=args.batch_size,
+    class_mode='binary')
+
 
 ## Model
 # TODO Write a simple convolutional neural network
+model_conv = km.Sequential()
+model_conv.add(kl.Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3), data_format="channels_last"))
+model_conv.add(kl.Activation('relu'))
+model_conv.add(kl.MaxPooling2D(pool_size=(2, 2)))
+
+model_conv.add(kl.Conv2D(32, (3, 3)))
+model_conv.add(kl.Activation('relu'))
+model_conv.add(kl.MaxPooling2D(pool_size=(2, 2)))
+
+model_conv.add(kl.Conv2D(64, (3, 3)))
+model_conv.add(kl.Activation('relu'))
+model_conv.add(kl.MaxPooling2D(pool_size=(2, 2)))
+
+model_conv.add(kl.Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model_conv.add(kl.Dense(64))
+model_conv.add(kl.Activation('relu'))
+model_conv.add(kl.Dropout(0.5))
+model_conv.add(kl.Dense(1))
+model_conv.add(kl.Activation('sigmoid'))
+
+model_conv.compile(loss='binary_crossentropy',
+                   optimizer='rmsprop',
+                   metrics=['accuracy'])
 
 
-model_conv =
 
 ## Learning
 
@@ -54,29 +96,32 @@ history = model_conv.fit_generator(train_generator, steps_per_epoch=N_train // a
 te = time.time()
 t_learning = te - ts
 
+
 ## Test
-# TODO Calculez l'accuracy de votre modèle sur le jeu d'apprentissage et sur le jeu de validation.
 
 print("Start predicting")
 ts = time.time()
-score_train =
-score_val =
+score_train = model_conv.evaluate_generator(train_generator, N_train / args.batch_size, verbose=1)
+score_val = model_conv.evaluate_generator(validation_generator, N_val / args.batch_size, verbose=1)
 te = time.time()
 t_prediction = te - ts
 
 
-
-args_str = "epochs_%d_batch_size_%d" %(args.epochs, args.batch_size)
-
 ## Save Model
 
 # TODO Save model in model folder
+# Pour avoir un nom generique
+args_str = "epochs_%d_batch_size_%d" %(args.epochs, args.batch_size)
+model_conv.save(args.model_dir + "/" + args_str + ".h5")
 
 
 ## Save results
-
 ## TODO Save results (learning time, prediction time, train and test accuracy, history.history object) in result folder
-
+print("Save results")
+results = vars(args)  #instancie un dict avec les args
+results.update({"t_learning": t_learning, "t_prediction": t_prediction, "accuracy_train": score_train,
+                 "accuracy_val": score_val, "history" : history.history})
+pickle.dump(results, open(args.results_dir + "/" + args_str + ".pkl", "wb"))
 
 
 
--- CodeDevelopment/script/prediction.py
+++ CodeDevelopment/script/prediction.py
@@ -15,9 +15,19 @@ print(MODE)
 
 ## Argument
 import argparse
-# TODO Write here the parameters that can be given as inputs to the algorithm.
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', type=str, default=)
+parser.add_argument('--epochs', type=int, default=2)
+parser.add_argument('--batch_size', type=int, default=20)
+
+DATA_DIR = '/Users/cecile/Documents/INSA/5A/AI-Frameworks/CodeDevelopment'
+parser.add_argument('--data_dir', type=str,
+                    default=DATA_DIR+"/data/")
+parser.add_argument('--results_dir', type=str,
+                    default=DATA_DIR+"/results/")
+parser.add_argument('--model_dir', type=str,
+                    default=DATA_DIR+"/model/")
+
 args = parser.parse_args()
 
 
@@ -29,24 +39,34 @@ args = parser.parse_args()
 img_width = 150
 img_height = 150
 
-
 data_dir_test = args.data_dir+'/test'
 N_test = len(os.listdir(data_dir_test+"/test"))
 
-test_datagen = kpi.ImageDataGenerator()
-test_generator = test_datagen.flow_from_directory()
+test_datagen = kpi.ImageDataGenerator(rescale=1. / 255)
+test_generator = test_datagen.flow_from_directory(
+    data_dir_test,
+    target_size=(img_height, img_width),
+    batch_size=args.batch_size,
+    class_mode=None,
+    shuffle=False)
+
 
 
 ## Download model
 # Todo Download model saved in learning script.
 args_str = "epochs_%d_batch_size_%d" %(args.epochs, args.batch_size)
-
+model_conv = km.load_model(args.model_dir + "/" + args_str + ".h5")
 
 ## Prediction
-# Todo Generate prediction.
+test_prediction = model_conv.predict_generator(test_generator, N_test // args.batch_size, verbose=1)
 
 ## Save prediction in csv
-# TODO Save the results in a csv file.
+images_test = test_generator.filenames
+classes = [int(t>0.5) for t in test_prediction]
+
+array = np.vstack((images_test, test_prediction[:,0], classes)).T
+df = pd.DataFrame(array, columns=["filename","probabilities","classes"])
+df.to_csv(args.results_dir+"/prediction_"+args_str+".csv", index=False)
 
 
 
